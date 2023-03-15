3aa68a992053e0e357b831ac397310d85bb1b71f
clark1816 <33646776+clark1816@users.noreply.github.com>
2023-03-14 22:59:00 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+
+st.title("clarks journey through tech")
+
+st.subheader("My First Project")
+st.write("My first project was a game similar to doodle jump where called space invaders which I deployed to the app store. This app was made with unite which uses C# so this game thought me C#.")
+st.write("This is my first game [space invaders](https://apps.apple.com/sa/app/galaxy-space-jumper/id1547356153)")
+
+st.subheader("My Second Project")
+st.write("My second project was for a class called intro to software engineering it was box stacking game called box stackers. This game was made with unity and C# as well and also helped me solidfy my knowlege of C# and ios deployment.")
+st.write("This my second game [box stackers](https://apps.apple.com/sa/app/box-stackerz/id1552616950)")
+
+st.subheader("My Third Project")
+st.write("For my third project I made a iOS app and website. This project was for another class I took which was based on learning user design. My website could defenitly use some updating now that my skills are better but it also help to show how far I have come.")
+st.write("This is my fitness website [fitness website](https://durofitnessapp.com/)")
+st.write("This is my fitness app [fitness app](https://apps.apple.com/sa/app/duro-fitness/id1598757712)")
+
+st.subheader("My Fourth Project")
+st.write("My sixth project was a stock screener project that I worked on for a whole year all by myself. This project taught me how to use python and how to use the yfinance API. This project was also helpful to learn how to use python and how to use the yfinance API. This project helped me to get really good at python and start thinking as a programmer.")
+st.write("This is my stock screener [stock screener]('https://clark1816-findashboard-dashboard-i8j87r.streamlit.app/')")
+
+st.subheader("My Fifth Project")
+st.write("For my fourth project I made a crypto currency, and many NFTS. I used the ethereum blockchain and solidity to make this project. I also used the open sea API to make my NFTS. This project was helpful to learn about web3 when it was big and also helped me learn about the ethereum blockchain.")
+st.write("This is my crypto currency [crypto currency](https://poocoin.app/tokens/0xbbc80efc61f419740ee33a5fa79154e26555d8f8)")
+st.write("This is my NFT [NFT](https://opensea.io/Clark1816/created)")
+
+st.subheader("My Sixth Project")
+st.write("For my fifth project was a website for Mt. Lebanon Shoe Repair this project taguht me how to use wordpress and how to make a website. This project was also helpful to learn how to use wordpress and how to make a website.")
+st.write("This is my website [website](https://mtlebanonshoerepair.com/)")
+
+
+st.subheader("My Seventh Project")
+st.write("My sesventh project was a research project I did with Dr. Buxton. This project was a streamlit dashboard which would simulate particles in lattice. This prject was also what transitioned me into machine learning and artifical intelegence.")
+st.write("This is my lattice spring simulator [simulator](https://clark1816-latticespringmodel-2dlsmv2-rt8izu.streamlit.app/)")
+
+st.subheader("My Eighth Project")
+st.write("My eighth project was a open source trading bot. I used python and made a flask app. I also used this project to work on my front end skills. In this project I used many python libraries such as alpaca to place the market orders, TA-Lib to get the technical indicators, and many more.")
+st.write("This is my trading bot [trading bot](https://github.com/letstradebot)")
+
+st.subheader("My Ninth Project")
+st.write("My ninth project was for a hackathon I worked on this project with my friend Harry and we won the hackathon. We used machine learning in python to predict the amount of a unknown substance in a vat for a nail polish company. We used a neural network to predict the amount of the unknown substance.")
+st.write("This is my hackathon project [hackathon project](https://clark1816-rmuhackathonrepo-app-8wq1fz.streamlit.app/)")
+
+st.subheader("My Tenth Project")
+st.write("My tenth project was really cool and I am still working on it rigtht now. I am using a very complex search alogrith that is better than amazon's to reverse image search. I am using a neural network to do this and I am using a lot of python libraries such as tensorflow, keras, and many more. To make flask app where the user can upload an image and get a link to products that are similar to that image.")
+st.write("This is my reverse image search [reverse image search](https://nurosearch.xyz/)")
+
+st.subheader("My Eleventh Project")
+st.write("My eleventh project was a gauss jordan elimnation calculator. I used python and streamlit to make this project. This project was helpful to learn how to use streamlit and how to use python. This project helped me stay interested in linear algebra.")
+st.write("This is my gauss jordan elimnation calculator [gauss jordan elimnation calculator](https://clark1816-gauss-jordan-elim-app-fwcuqi.streamlit.app/)")
+
+st.subheader("My Twelfth Project")
+st.write("My twelth project was an AI website I have made a streamlit app and added many AI things that I need to help me on daily basis. This project is continuous and since I can program so quickly and easily using streamlit it acts as testing ground to see if I want to make it into a flask app. The only downside to streamlit is that it offers less customization thna a flask app does. I used this app to make a AI helpers such as book summarizer, AI helper and a AI video summarizer. I made a GPT-3 helped since I made this before chat-GPT existed.")
+st.write("This is my AI [Streamlit AI](https://clark1816-chatbotai.streamlit.app/)")
+
+st.subheader("My Thirteenth Project")
+st.write("My thirteenth project was a flask app that is a therapist using the new chat-gpt api")
+st.write("This is my therapist [therapist](https://therapo.xyz/)")
+
+st.subheader("My Fourteenth Project")
+st.write("My fourteenth project was a game that I made using unity so that I could load this game up and relax and chill out becuase coding can get a little stressful.")
+st.write("This is my game [game](https://clark1816.itch.io/amazing-racer)")
+
+st.subheader("Much More To Come...")
+st.write("Check out my github I have many small programs and helpful tools that helped me to create these projects. They are like miny projects that I made while making these major projects.")
+st.write("This is my github [github](https://github.com/clark1816)")
+
+st.subheader("Profile Website")
+st.write("This is my profile website [profile website](https://clarkmcquiston.000webhostapp.com/)")
